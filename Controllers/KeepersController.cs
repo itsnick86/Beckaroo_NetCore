dbbefcda6f31f41ef930e16984a3a73972346cd6
Nick Edwards <nickedwards@nicks-mbp.attlocal.net>
2020-04-23 21:09:28 -0400

    Added Keepers Controller and Index page.

---
+++ Controllers/KeepersController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Beckaroo_NetCore.Models;
+
+namespace Beckaroo_NetCore.Controllers
+{
+    public class KeepersController : Controller
+    {
+        private readonly ILogger<KeepersController> _logger;
+
+        public KeepersController(ILogger<KeepersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
