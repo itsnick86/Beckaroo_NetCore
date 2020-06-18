d221bd2d8f25db450336ba5a15f73cf6f11eaeae
Nick Edwards <nickedwards@nicks-mbp.attlocal.net>
2020-06-18 08:16:27 -0400

    Added a Blog Model and Animal Model.Also made slight adjustments to the Animal Form layout.

---
+++ Models/AnimalModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Beckaroo_NetCore.Models
+{
+    public class AnimalModel
+    {
+        public int AnimalID { get; set; }
+        public string AnimalName { get; set; }
+        public string AnimalDescription { get; set; }
+        public string AnimalPictureMain { get; set; }
+        public string AnimalPictureSecondary { get; set; }
+    }
+}
