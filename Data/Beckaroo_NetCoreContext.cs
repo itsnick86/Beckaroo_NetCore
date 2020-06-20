cf0734367dbfd6cf0a0733fe13514fede7fab386
Nick Edwards <nickedwards@nicks-mbp.attlocal.net>
2020-06-20 18:16:15 -0400

    Scaffolded pages for the Animal and Blog tables and forms. Completed initial db migration.

---
+++ Data/Beckaroo_NetCoreContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Beckaroo_NetCore.Models;
+
+namespace Beckaroo_NetCore.Data
+{
+    public class Beckaroo_NetCoreContext : DbContext
+    {
+        public Beckaroo_NetCoreContext(DbContextOptions<Beckaroo_NetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animal { get; set; }
+        public DbSet<Blog> Blog { get; set; }
+    }
+}
---
+++ Migrations/20200620214257_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Beckaroo_NetCore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animal",
+                columns: table => new
+                {
+                    AnimalID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ImageMain = table.Column<string>(nullable: true),
+                    ImageSecondary = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animal", x => x.AnimalID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    BlogID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    PublishDate = table.Column<DateTime>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.BlogID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animal");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}
