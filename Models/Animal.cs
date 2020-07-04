53325ef8bbeff5644ab16b1a1543932c0a7aedb6
Nick Edwards <nickedwards@nicks-mbp.attlocal.net>
2020-07-04 16:40:01 -0400

    Updated Animal Index and Create views. Update Animal Model to accept image files. Modified Animal controller to Create a new Animal.

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,4 +1,6 @@
 using System;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace Beckaroo_NetCore.Models
@@ -10,9 +12,16 @@ namespace Beckaroo_NetCore.Models
         public string Description { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name="Birthday")]
         public DateTime DateOfBirth { get; set; }
         public string Species { get; set; }
+        [Display(Name="Main Image")]
         public string ImageMain { get; set; }
+        [NotMapped]
+        public IFormFile ImageMainFile { get; set; }
+        [Display(Name="Modal Image")]
         public string ImageSecondary { get; set; }
+        [NotMapped]
+        public IFormFile ImageSecondaryFile { get; set; }
     }
 }
