d0f74bff32bb7ac21b18ab7c8dcd517bf4c47302
Nick Edwards <nickedwards@nicks-mbp.attlocal.net>
2020-07-04 11:28:53 -0400

    Added Login functionality to admin page. Updated rich text editor submission so that html is sent to the db.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,16 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Beckaroo_NetCore.Data;
 using Beckaroo_NetCore.Models;
-
 namespace Beckaroo_NetCore.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly Beckaroo_NetCoreContext _context;
         private readonly ILogger<AdminController> _logger;
 
-        public AdminController(ILogger<AdminController> logger)
+        public AdminController(
+            Beckaroo_NetCoreContext context
+            , ILogger<AdminController> logger
+            )
         {
+            _context = context;
             _logger = logger;
         }
 
@@ -23,19 +28,34 @@ namespace Beckaroo_NetCore.Controllers
             return View();
         }
 
-        public IActionResult AdminPanel()
+        public IActionResult LogIn([Bind("Username, Password")] Admin admin)
         {
-            return View("AdminPanel");
-        }
+            //Check if Username or Password isn't provided
+            if(admin.Username == null || admin.Password == null) 
+            {
+                return View("Index");
+            }
 
-        public IActionResult BlogForm()
-        {
-            return View("BlogForm");
-        }
+            //Find User in Database
+            var user = _context.Admin
+                .FirstOrDefault(m => m.Username == admin.Username);
 
-        public IActionResult AnimalForm()
-        {
-            return View("AnimalForm");
+            //Check if user is empty
+            if (user == null)
+            {
+                return View("Index");
+            }
+
+            //Validate Username and Password provided
+            if (user.Username == admin.Username && user.Password == admin.Password)
+            {
+                return RedirectToAction("Index", "Blogs");
+            }
+            else
+            {
+                return View("Index");
+            }
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/Admin.cs
+++ Models/Admin.cs
@@ -6,7 +6,7 @@ namespace Beckaroo_NetCore.Models
     public class Admin
     {
         public int AdminID { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
